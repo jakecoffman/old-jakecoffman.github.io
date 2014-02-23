5bf6fc4c381ffc2f3d54d6684b2fd983b4763b50
jake <jake@coffshire.com>
2014-02-23 14:28:03 -0600

    Migrating portfolio

---
+++ static/angular/app.js
@@ -0,0 +1,74 @@
+var app = angular.module('portfolio', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when("/", {
+		templateUrl: "/templates/home.html"
+	});
+	$routeProvider.when("/projects/:project", {
+		templateUrl: function(params) { return "/templates/projects/" + params.project + ".html" },
+		controller: 'ProjectCtl'
+	}).otherwise({
+		redirectTo: "/"
+	})
+}]);
+
+app.run(function($rootScope, $location, $anchorScroll, $routeParams, $window) {
+	//when the route is changed scroll to the proper element.
+	$rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+		$location.hash($routeParams.scrollTo);
+		$anchorScroll();
+	});
+
+	$rootScope.$on('$viewContentLoaded', function(event) {
+		$window._gaq.push(['_trackPageview', $location.path()]);
+	});
+});
+
+app.controller('MainCtl', ['$scope', '$http', function($scope, $http){
+	$scope.logos = logos;
+}]);
+
+app.controller('ProjectCtl', ['$scope', '$routeParams', function($scope, $routeParams){
+	$scope.desk = [{
+		href: '/static/desk/desk0.jpg'
+	},{
+		href: '/static/desk/desk1.jpg'
+	},{
+		href: '/static/desk/desk2.jpg'
+	},{
+		href: '/static/desk/desk5.jpg'
+	},{
+		href: '/static/desk/desk3.jpg'
+	},{
+		href: '/static/desk/desk4.jpg'
+	}];
+
+	$scope.project = $routeParams.project;
+	$scope.all = ['alarm', 'desk', 'flask_tutorial', 'gameoflife', 'gorunner', 'jobs', 'taskpy'];
+}]);
+
+var logos = [{
+	image: "python.png",
+	alt: "python"
+},{
+	image: "git.png",
+	alt: "git"
+},{
+	image: "angularjs.png",
+	alt: "angularjs"
+},{
+	image: "django.png",
+	alt: "django"
+},{
+	image: "js.png",
+	alt: "javascript"
+},{
+	image: "java.png",
+	alt: "java"
+},{
+	image: "go.png",
+	alt: "go"
+},{
+	image: "ruby.gif",
+	alt: "ruby"
+}];
